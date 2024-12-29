72cb153a2ed48a68860c92a386a5b26521476a84
raccoonRyu <rjhwind94@gmail.com>
2024-12-30 08:53:44 +0900

    최초 커밋

---
+++ CH01_WhatIsPython/ex0.py
@@ -0,0 +1,19 @@
+a = "Python"
+print(a)
+
+a = 3
+if a > 1 :
+    print("a is greater than i")
+
+for a in [1, 2, 3] :
+    print(a)
+
+i = 0
+while i < 3 :
+    i = i + 1
+    print(i)
+
+def add(a, b) :
+    return a + b
+
+print(add(3, 4)) # 7 출력
---
+++ CH01_WhatIsPython/hello.py
@@ -0,0 +1,3 @@
+# hello.py
+# 샵으로 시작하는 문장은 python의 주석이다.
+print("Hello World")
